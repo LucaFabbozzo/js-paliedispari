90c28ced0681db7677ee79740ccb0350b71b96bb
LucaFabbozzo <lucafabbozzo@gmail.com>
2022-10-19 09:10:39 -0500

    corrections divide it into two folders

--- Palindroma/js/script.js
+++ Palindroma/js/script.js
@@ -2,13 +2,6 @@
 // **Palidroma**
 // Chiedere all’utente di inserire una parola
 // Creare una funzione per capire se la parola inserita è palindroma
-// **Pari e Dispari**
-// L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
-// Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
-// Sommiamo i due numeri
-// Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
-// Dichiariamo chi ha vinto.
-// **Consigli del giorno**
 // 1. Scriviamo sempre in italiano i passaggi che vogliamo fare
 // 2. Scriviamo sempre solo un pezzetto di codice alla volta, se funziona allora andiamo avanti.
 
@@ -22,9 +15,9 @@ const parolaInvertita = invertireParola(parolaRichiesta);
 //faccio il mio controllo se la parola richiesta è uguale alla parola invertita
 
 if(parolaRichiesta === parolaInvertita){
-    console.log('è Palidroma');
+    console.log('è Palindroma');
   } else {
-    console.log('non è Palidroma');
+    console.log('non è Palindroma');
 }
 
 // funzione con ciclo for per poter invertire la parola
@@ -35,7 +28,7 @@ function invertireParola(string){
   for(let i = string.length - 1; i >= 0; i--) {
     parolaInversa += string[i];
   }
-  
+
   return parolaInversa
 }
 
