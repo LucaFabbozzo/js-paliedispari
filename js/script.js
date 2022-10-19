7915af4f8552e2fa4d1cbe9f91bc8fd7d7e14ebe
LucaFabbozzo <lucafabbozzo@gmail.com>
2022-10-19 09:04:54 -0500

    [add] Palidroma logic

--- js/script.js
+++ js/script.js
@@ -12,4 +12,31 @@
 // 1. Scriviamo sempre in italiano i passaggi che vogliamo fare
 // 2. Scriviamo sempre solo un pezzetto di codice alla volta, se funziona allora andiamo avanti.
 
-console.log('ciaoooo')
+
+// Chiedere all’utente di inserire una parola
+const parolaRichiesta = prompt('Inserisci una parola').toLowerCase();
+
+//creo una constante che mi dara il valore della parola invertita con una funzione
+const parolaInvertita = invertireParola(parolaRichiesta);
+
+//faccio il mio controllo se la parola richiesta è uguale alla parola invertita
+
+if(parolaRichiesta === parolaInvertita){
+    console.log('è Palidroma');
+  } else {
+    console.log('non è Palidroma');
+}
+
+// funzione con ciclo for per poter invertire la parola
+
+function invertireParola(string){
+  let parolaInversa = '';
+
+  for(let i = string.length - 1; i >= 0; i--) {
+    parolaInversa += string[i];
+  }
+  
+  return parolaInversa
+}
+
+
